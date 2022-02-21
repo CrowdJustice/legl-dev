0ead088fbfe76fa13f17b8c4b0efeec00e775b79
phil-bell <philhabell@gmail.com>
2022-02-21 22:01:25 +0000

    removing support for non verbose mode and over all simplifying code

--- legl_dev/main.py
+++ legl_dev/main.py
@@ -16,7 +16,6 @@ def docker_command(mutagen):
 
 @app.command(help="Start the dev enviroment")
 def start(
-    verbose: bool = typer.Option(True, help="Run the command in verbose mode"),
     mutagen: bool = typer.Option(False, help="Run docker with Mutagen"),
     https: bool = typer.Option(False, help="Run server in HTTPS mode"),
     worker: bool = typer.Option(False, help="Run server with worker"),
@@ -30,9 +29,6 @@ def start(
         steps.add(
             Command(
                 command=f"{docker_command(mutagen)} exec backend python manage.py runsslserver 0.0.0.0:443",
-                desc="Starting django over SSL",
-                log_file="start_django_ssl",
-                verbose=verbose,
             )
         )
     else:
@@ -41,9 +37,6 @@ def start(
         steps.add(
             Command(
                 command=f"{docker_command(mutagen)} up backend",
-                desc="Starting docker compose",
-                log_file="start_docker_compose",
-                verbose=verbose,
             )
         )
     if worker:
@@ -53,27 +46,15 @@ def start(
             [
                 Command(
                     command="rabbitmq-server",
-                    desc="Booting the rabbitMQ server with default config",
-                    log_file="run_rabbitmq",
-                    verbose=verbose,
                 ),
                 Command(
                     command="pm2 start ./legl_dev/pm2.config.js",
-                    desc="Spinning up process manager for celery queue consumers",
-                    log_file="run_pm2",
-                    verbose=verbose,
                 ),
                 Command(
                     command="pm2 log",
-                    desc="Starting pm2 logger",
-                    log_file="pm2_logger",
-                    verbose=verbose,
                 ),
                 Command(
                     command="rabbitmq-diagnostics log_tail_stream",
-                    desc="Starting rabbitmq logger",
-                    log_file="rabbitmq_logger",
-                    verbose=verbose,
                 ),
             ]
         )
@@ -84,9 +65,6 @@ def start(
         steps.add(
             Command(
                 command=f"{docker_command(mutagen)} up frontend",
-                desc="Starting webpack",
-                log_file="start_webpack",
-                verbose=verbose,
             )
         )
 
@@ -95,7 +73,6 @@ def start(
 
 @app.command(help="Rebuild the local enviroment")
 def build(
-    verbose: bool = typer.Option(False, help="Run the command in verbose mode"),
     mutagen: bool = typer.Option(False, help="Run docker with Mutagen"),
     cache: bool = typer.Option(True, help="Drop the database and create a fresh one"),
 ) -> None:
@@ -104,51 +81,26 @@ def build(
     steps = Steps(
         steps=[
             Command(
-                log_file="building_docker_image",
-                desc="Building docker image",
                 command=f"{docker_command(mutagen)} build {extra_args}",
-                verbose=verbose,
             ),
         ],
     )
     steps.add(
         [
+            Command(command=f"{docker_command(mutagen)} up -d"),
             Command(
-                log_file="start_docker",
-                desc="starting docker compose",
-                command=f"{docker_command(mutagen)} up -d",
-                verbose=verbose
-            ),
-            Command(
-                log_file="run_migrations",
-                desc="Running migrations",
                 command=f"{docker_command(mutagen)} exec backend python manage.py migrate",
-                verbose=verbose,
             ),
             Command(
-                log_file="flush",
-                desc="Flushing database data",
                 command=f"{docker_command(mutagen)} exec backend python manage.py flush --noinput",
-                verbose=verbose,
             ),
             Command(
-                log_file="create_factories",
-                desc="Creating factory data",
                 command=f"{docker_command(mutagen)} exec backend python manage.py run_factories",
-                verbose=verbose,
             ),
             Command(
-                log_file="create_emails",
-                desc="Seeding emails",
                 command=f"{docker_command(mutagen)} exec backend python manage.py seed_emails",
-                verbose=verbose,
             ),
-            Command(
-                log_file="stop_docker",
-                desc="Stopping docker compose",
-                command=f"{docker_command(mutagen)} stop",
-                verbose=verbose
-            )
+            Command(command=f"{docker_command(mutagen)} stop"),
         ]
     )
     steps.run()
@@ -158,18 +110,42 @@ def build(
 
 @app.command(help="Run the local pytest unit tests")
 def pytest(
-    verbose: bool = typer.Option(True, help="Run the command in verbose mode",),
     mutagen: bool = typer.Option(False, help="Run docker with Mutagen"),
-    full_diff: bool = typer.Option(False, help="Show full diff in errors",),
-    create_db: bool = typer.Option(False, help="Recreates the test database",),
-    last_failed: bool = typer.Option(False, help="Run the last failed tests",),
-    warnings: bool = typer.Option(False, help="Toggle warnings in output",),
-    gui: bool = typer.Option(True, help="Toggle the output gui",),
-    snapshot_update: bool = typer.Option(False, help="update snapshots",),
-    show_capture: bool = typer.Option(False, help="show captured stdout",),
-    parallel: bool = typer.Option(False, help="run tests in parallel",),
+    full_diff: bool = typer.Option(
+        False,
+        help="Show full diff in errors",
+    ),
+    create_db: bool = typer.Option(
+        False,
+        help="Recreates the test database",
+    ),
+    last_failed: bool = typer.Option(
+        False,
+        help="Run the last failed tests",
+    ),
+    warnings: bool = typer.Option(
+        False,
+        help="Toggle warnings in output",
+    ),
+    gui: bool = typer.Option(
+        True,
+        help="Toggle the output gui",
+    ),
+    snapshot_update: bool = typer.Option(
+        False,
+        help="update snapshots",
+    ),
+    show_capture: bool = typer.Option(
+        False,
+        help="show captured stdout",
+    ),
+    parallel: bool = typer.Option(
+        False,
+        help="run tests in parallel",
+    ),
     all_logs: bool = typer.Option(
-        False, help="show print outputs instead of just logs",
+        False,
+        help="show print outputs instead of just logs",
     ),
     path: str = typer.Argument(
         "",
@@ -190,30 +166,21 @@ def pytest(
     steps = Steps(
         steps=[
             Command(
-                log_file="run_unit_tests",
-                desc="Running unit tests",
                 command=f"{docker_command(mutagen)} exec backend pytest --html=unit_test_results.html {extra_args} {path}",
-                verbose=verbose,
             ),
         ]
     )
     if gui:
         steps.add(
             Command(
-                log_file="open_unit_test_results",
-                desc="Opening test results",
                 command=f"open ./unit_test_results.html",
-                verbose=verbose,
             )
         )
 
     if snapshot_update:
         steps.add(
             Command(
-                log_file="black",
-                desc="Blacking codebase",
                 command=f"{docker_command(mutagen)} exec backend black .",
-                verbose=verbose,
             )
         )
     steps.run()
@@ -221,7 +188,6 @@ def pytest(
 
 @app.command(help="Format the code with isort, black and prettier")
 def format(
-    verbose: bool = typer.Option(False, help="Run the command in verbose mode"),
     mutagen: bool = typer.Option(False, help="Run docker with Mutagen"),
     push: bool = typer.Option(False, help="Also push the changes to the repo"),
 ):
@@ -229,22 +195,13 @@ def format(
     steps = Steps(
         steps=[
             Command(
-                log_file="isort",
-                desc="sorting python imports",
                 command=f"{docker_command(mutagen)} exec backend isort .",
-                verbose=verbose,
             ),
             Command(
-                log_file="black",
-                desc="blacking codebase",
                 command=f"{docker_command(mutagen)} exec backend black .",
-                verbose=verbose,
             ),
             Command(
-                log_file="pretty",
-                desc="prettifiying codebase",
                 command=f"{docker_command(mutagen)} exec frontend yarn run format:prettier",
-                verbose=verbose,
             ),
         ]
     )
@@ -252,16 +209,10 @@ def format(
         steps.add(
             [
                 Command(
-                    log_file="git_stage",
-                    desc="staging changes",
                     command="git stage .",
-                    verbose=verbose,
                 ),
                 Command(
-                    log_file="git_commit",
-                    desc="commiting changes",
                     command='git commit -m "formatting"',
-                    verbose=verbose,
                 ),
                 Command(
                     log_file="git_push", desc="pushing changes", command="git push"
@@ -272,18 +223,12 @@ def format(
 
 
 @app.command(help="Open Cypress e2e tests")
-def cypress(
-        verbose: bool = typer.Option(True, help="Run the command in verbose mode"),
-        mutagen: bool = typer.Option(False, help="Run docker with Mutagen")
-    ):
+def cypress(mutagen: bool = typer.Option(False, help="Run docker with Mutagen")):
 
     steps = Steps(
         steps=[
             Command(
-                log_file="cypress",
-                desc="running cypress",
                 command=f"{docker_command(mutagen)} exec frontend yarn run cypress open",
-                verbose=verbose,
             )
         ]
     )
@@ -292,7 +237,6 @@ def cypress(
 
 @app.command(help="Create and run migrations")
 def migrate(
-    verbose: bool = typer.Option(False, help="Run the command in verbose mode"),
     mutagen: bool = typer.Option(False, help="Run docker with Mutagen"),
     merge: bool = typer.Option(False, help="Run a migration merge first"),
     make: bool = typer.Option(False, help="Run makemigrations before migrating"),
@@ -305,36 +249,24 @@ def migrate(
     if merge:
         steps.add(
             Command(
-                log_file="merge_migrations",
-                desc="Merging migration",
                 command=f"{docker_command(mutagen)} exec backend python manage.py makemigrations --merge",
-                verbose=True,
             ),
         )
     if make:
         steps.add(
             Command(
-                log_file="make_migrations",
-                desc="Making migrations",
                 command=f"{docker_command(mutagen)} exec backend python manage.py makemigrations",
-                verbose=verbose,
             ),
         )
     if run:
         steps.add(
             Command(
-                log_file="run_migrations",
-                desc="Running migrations",
                 command=f"{docker_command(mutagen)} exec backend python manage.py migrate",
-                verbose=verbose,
             ),
         )
     steps.add(
         Command(
-            log_file="black",
-            desc="blacking codebase",
             command=f"{docker_command(mutagen)} exec backend black .",
-            verbose=verbose,
         ),
     )
     steps.run()
@@ -342,7 +274,6 @@ def migrate(
 
 @app.command(help="Clean out and create new factories")
 def factories(
-    verbose: bool = typer.Option(False, help="Run the command in verbose mode"),
     mutagen: bool = typer.Option(False, help="Run docker with Mutagen"),
     emails: bool = typer.Option(True, help="Generate factory emails"),
 ):
@@ -350,42 +281,28 @@ def factories(
     steps = Steps(
         steps=[
             Command(
-                log_file="flush",
-                desc="Flushing",
                 command=f"{docker_command(mutagen)} exec backend python manage.py flush --noinput",
-                verbose=verbose,
             ),
             Command(
-                log_file="create_factories",
-                desc="Creating factory data",
                 command=f"{docker_command(mutagen)} exec backend python manage.py run_factories",
-                verbose=verbose,
             ),
         ],
     )
     if emails:
         steps.add(
             Command(
-                log_file="create_emails",
-                desc="Seeding emails",
                 command=f"{docker_command(mutagen)} exec backend python manage.py seed_emails",
-                verbose=verbose,
             ),
         )
     steps.run()
 
 
 @app.command(help="Cleans out git branches")
-def gitclean(
-    verbose: bool = typer.Option(False, help="Run the command in verbose mode"),
-):
+def gitclean():
     steps = Steps(
         steps=[
             Command(
-                log_file="gitclean",
-                desc="Cleaning out git branches",
                 command='git branch --merged | egrep -v "(^\\*|master|dev)" | xargs git branch -d',
-                verbose=verbose,
                 shell=True,
             ),
         ]
@@ -395,23 +312,16 @@ def gitclean(
 
 @app.command(help="Run JS unit tests")
 def jstest(
-    verbose: bool = typer.Option(True, help="Run the command in verbose mode"),
     mutagen: bool = typer.Option(False, help="Run docker with Mutagen"),
 ):
 
     steps = Steps(
         steps=[
             Command(
-                log_file="jstest",
-                desc="Running JS unit tests",
                 command=f"{docker_command(mutagen)} exec frontend yarn run test",
-                verbose=verbose,
             ),
             Command(
-                log_file="js_code_cov",
-                desc="Opening code coverage",
                 command="open js-test-results/index.html",
-                verbose=verbose,
             ),
         ]
     )
