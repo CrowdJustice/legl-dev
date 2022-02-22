0ba99151b5a2f5375d4d048a43e61886397fa0dd
phil-bell <philhabell@gmail.com>
2022-02-22 15:39:42 +0000

    removing test covering removed code

--- legl_dev/tests/test_command.py
+++ legl_dev/tests/test_command.py
@@ -11,8 +11,3 @@ def test_run_in_verbose_mode(run):
     run.assert_called_with(["echo", "test"], universal_newlines=True, shell=False)
 
 
-@mock.patch("legl_dev.command.yaspin")
-def test_run_not_in_verbose_mode(yaspin):
-    command = Command(log_file="test", desc="test", command="echo test", verbose=False)
-    command.run()
-    yaspin.assert_called_with(text="test", color="yellow")
