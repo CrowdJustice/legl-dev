933f699f59fb0ac286816e5a2d24de91943dd3a0
phil-bell <philhabell@gmail.com>
2022-03-03 13:16:43 +0000

    adding --rm to remove container after run

--- legl_dev/main.py
+++ legl_dev/main.py
@@ -39,17 +39,17 @@ def build(
     steps.add(
         [
             Command(command=f"docker compose up -d"),
-            Command(command=(f"docker compose run backend python manage.py migrate")),
+            Command(command=(f"docker compose run --rm backend python manage.py migrate")),
             Command(
                 command=(
-                    f"docker compose run backend python manage.py flush --noinput"
+                    f"docker compose run --rm backend python manage.py flush --noinput"
                 )
             ),
             Command(
-                command=(f"docker compose run backend python manage.py run_factories")
+                command=(f"docker compose run --rm backend python manage.py run_factories")
             ),
             Command(
-                command=(f"docker compose run backend python manage.py seed_emails")
+                command=(f"docker compose run --rm backend python manage.py seed_emails")
             ),
             Command(command=f"docker compose stop"),
         ]
@@ -193,7 +193,7 @@ def migrate(
         )
     if run:
         steps.add(
-            Command(command=(f"docker compose run backend python manage.py migrate")),
+            Command(command=(f"docker compose run --rm backend python manage.py migrate")),
         )
     steps.run()
 
@@ -207,18 +207,18 @@ def factories(
         steps=[
             Command(
                 command=(
-                    f"docker compose run backend python manage.py flush --noinput"
+                    f"docker compose run --rm backend python manage.py flush --noinput"
                 )
             ),
             Command(
-                command=(f"docker compose run backend python manage.py run_factories")
+                command=(f"docker compose run --rm backend python manage.py run_factories")
             ),
         ],
     )
     if emails:
         steps.add(
             Command(
-                command=(f"docker compose run backend python manage.py seed_emails")
+                command=(f"docker compose run --rm backend python manage.py seed_emails")
             ),
         )
     steps.run()
